60d85be8d389cbdac227729d86c592c127a7d659
Victor Ian Bautista <victor@mexiton.com>
2018-02-08 12:18:48 +0800

    Iniital commit

--- bootstrap-demo/server.js
+++ bootstrap-demo/server.js
@@ -1,8 +1,24 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+/*eslint valid-jsdoc: "off"*/
+/*eslint-env es6*/
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+'use strict';
+const port = process.env.PORT || 1337;
+
+const app = require('express')();
+const parser = require('body-parser');
+const os = require('os');
+
+// middleware configuration
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+
+let listener = app.listen(port, function () {
+  console.log('http://' + os.hostname() + ':' + listener.address().port);
+  if (process.env.NODE_ENV === 'development') {
+    console.log('DEV mode');
+  }
+});
+
+app.get('/', function (request, response) {
+  response.send('Login listening on http://' + os.hostname() + ':' + listener.address().port);
+});
